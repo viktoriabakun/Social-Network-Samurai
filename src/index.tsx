8f4386ccb077051330c92e3150922a76684520df
viktoriabakun <viktoriabakun00@gmail.com>
2020-08-03 16:18:35 +0300

    lesson 37 done. store, state, OOP, refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,25 @@
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
+import store, {RootStateType} from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, RootStateType, updateNewPostText} from './redux/state'
 import {BrowserRouter} from "react-router-dom";
 
-const rerenderEntireTree = () => {
+const rerenderEntireTree = (state: RootStateType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={store.getState()}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireTree();
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 
 serviceWorker.unregister();
