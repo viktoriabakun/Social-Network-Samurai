9f1f72437ea946bcbf3ba4bf7ff985aff4c01b1f
viktoriabakun <viktoriabakun00@gmail.com>
2020-10-24 09:35:59 +0300

    lesson 47 DONE: do a copy of state for dialogs-reducer

--- src/redux/dialogs-reducer.tsx
+++ src/redux/dialogs-reducer.tsx
@@ -27,15 +27,18 @@ let initialState: MessagesPage = {
 
 const dialogsReducer = (state: MessagesPage = initialState, action: DialogsActionType) => {
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
+        case UPDATE_NEW_MESSAGE_BODY: {
+            let stateCopy = {...state}
+            stateCopy.newMessageBody = action.body;
+            return stateCopy;
+        }
 
         case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messages.push({id: '6', message: body});
-            return state;
+            let stateCopy = {...state}
+            let body = stateCopy.newMessageBody;
+            stateCopy.newMessageBody = '';
+            stateCopy.messages.push({id: '6', message: body});
+            return stateCopy;
 
         default:
             return state;
