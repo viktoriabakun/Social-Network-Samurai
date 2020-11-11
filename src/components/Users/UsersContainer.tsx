06fcd0daa493a0cfaebfcdf66973767d743081aa
viktoriabakun <viktoriabakun00@gmail.com>
2020-11-11 13:14:23 +0300

    lesson 53: done

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {connect} from "react-redux";
-import Users from "./UsersCC";
-import {RootStateType, UserObjType, UsersType} from "../../redux/store";
+import Users from "./Users";
+import {RootStateType, UserObjType} from "../../redux/store";
 import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
-import { Dispatch } from 'redux';
+import {Dispatch} from 'redux';
 
 
 type MapStateType = {
