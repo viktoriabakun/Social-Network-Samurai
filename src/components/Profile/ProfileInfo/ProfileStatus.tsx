88281b19bed2db04f01f23853aedad39a870b17a
viktoriabakun <viktoriabakun00@gmail.com>
2021-01-15 22:32:23 +0300

    lesson 75: Login. Redux Form. DONE

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -4,9 +4,13 @@ type ProfileStatusProps = {
     status: string
     updateStatus: (status: string) => void
 }
+type ProfileStatusState = {
+    editMode: boolean
+    status: string
+}
 
 class ProfileStatus extends React.Component<ProfileStatusProps> {
-    state = {
+    state: ProfileStatusState = {
         editMode: false,
         status: this.props.status
     }
@@ -30,18 +34,27 @@ class ProfileStatus extends React.Component<ProfileStatusProps> {
         })
     }
 
+    componentDidUpdate(prevProps: ProfileStatusProps, prevState: ProfileStatusState) {
+     if(prevProps.status !== this.props.status){
+         this.setState({
+             status: this.props.status
+         })
+     }
+        console.log('componentDidUpdate')
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status || 'enter your status'}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
                     <input onChange={this.onStatusChange}
-                        autoFocus={true}
+                           autoFocus={true}
                            onBlur={this.deactivateEditMode}
                            value={this.state.status}/>
                 </div>
