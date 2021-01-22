65eeb7188fcf43e939e71515f8cfe2122473e4c7
viktoriabakun <viktoriabakun00@gmail.com>
2021-01-22 23:07:59 +0300

    lesson 78: now we can login!

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,27 +2,34 @@ import React from "react";
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {Input} from "../common/FormsControls/FormControls";
 import {required} from "../../utils/validators/validator";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
 
 type FormDataType = {
-    login: string,
+    email: string,
     password: string,
     rememberMe: boolean
+}
+
+type LoginType = {
+    login: (email: string, password: string, rememberMe: boolean) => void
 }
 
 const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'Login'}
-                       name={'login'}
+                <Field placeholder={'Email'}
+                       name={'email'}
                        component={Input}
                        validate={[required]}
                 />
             </div>
             <div>
-                <Field placeholder={'password'}
+                <Field placeholder={'Password'}
                        name={'password'}
                        component={Input}
+                       type={'password'}
                        validate={[required]}
                 />
             </div>
@@ -41,9 +48,9 @@ const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
 const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
 
 
-const Login = () => {
+const Login = (props: LoginType) => {
     const onSubmit = (formData: FormDataType) => {
-        console.log(formData)
+        props.login(formData.email, formData.password, formData.rememberMe)
     }
     return (
         <div>
@@ -53,4 +60,4 @@ const Login = () => {
     )
 };
 
-export default Login;
+export default connect(null, {login}) (Login);
--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,9 +1,16 @@
 import {UsersType} from "./store";
 import {Dispatch} from "redux";
 import {authAPI} from "../api/api";
+import {ThunkDispatch} from 'redux-thunk';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
+export type InitialStateType = {
+    userId: null | string,
+    email: null | string,
+    login: null | string,
+    isAuth: boolean
+}
 
 let initialState: any = {
     userId: null,
@@ -19,7 +26,7 @@ const authReducer = (state: UsersType = initialState, action: any) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data,
+                ...action.payload,
                 isAuth: true,
             }
 
@@ -29,14 +36,34 @@ const authReducer = (state: UsersType = initialState, action: any) => {
     }
 }
 
-export const setAuthUserData = (userId: number, email: string, login: string) => ({type: SET_USER_DATA, data: {userId, email, login}})
+export const setAuthUserData = (userId: number|null, email: string|null, login: string|null, isAuth: boolean) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}})
 export const getAuthUserData = () => (dispatch: Dispatch) => {
      authAPI.me()
         .then(response => {
 
             if(response.data.resultCode === 0){
-                let {id, email, login } = response.data.data;
-                dispatch(setAuthUserData(id, email, login))
+                let {id, email, login, isAuth } = response.data.data;
+                dispatch(setAuthUserData(id, email, login, isAuth))
+            }
+        });
+}
+
+export const login = (email: string, password: string, rememberMe: boolean) => (dispatch:  ThunkDispatch<InitialStateType, {}, any>) => {
+    authAPI.login(email,password,rememberMe)
+        .then(response => {
+
+            if(response.data.resultCode === 0){
+               dispatch(getAuthUserData())
+            }
+        });
+}
+
+export const logout = () => (dispatch:  ThunkDispatch<InitialStateType, {}, any>) => {
+    authAPI.logout()
+        .then(response => {
+
+            if(response.data.resultCode === 0){
+                dispatch(setAuthUserData(null, null, null, false))
             }
         });
 }
