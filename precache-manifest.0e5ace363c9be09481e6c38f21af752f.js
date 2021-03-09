60b92dfba4947cb502241cd9b01be6598c1d9c00
viktoriabakun <viktoriabakun00@gmail.com>
2021-03-10 00:18:53 +0300

    Updates

---
+++ precache-manifest.0e5ace363c9be09481e6c38f21af752f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaff8734d3d7e3f02a3b5a7b91840df",
+    "url": "/Social-Network-Samurai/index.html"
+  },
+  {
+    "revision": "b660cfb5fc6b9ef2c7d8",
+    "url": "/Social-Network-Samurai/static/css/main.5e53feba.chunk.css"
+  },
+  {
+    "revision": "8879af58e8fe1c50e3ab",
+    "url": "/Social-Network-Samurai/static/js/2.f46c2355.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Social-Network-Samurai/static/js/2.f46c2355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b660cfb5fc6b9ef2c7d8",
+    "url": "/Social-Network-Samurai/static/js/main.db235fd1.chunk.js"
+  },
+  {
+    "revision": "f1d7e3ea9d34afbd0f6b",
+    "url": "/Social-Network-Samurai/static/js/runtime-main.275cbf43.js"
+  },
+  {
+    "revision": "28c6fb3b1b6fde66a0fe63371e7f9ab3",
+    "url": "/Social-Network-Samurai/static/media/preloader.28c6fb3b.svg"
+  }
+]);
